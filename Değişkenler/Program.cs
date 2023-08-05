ad544bf8bcdbd9b0ad7e3012a6053db2f9afe48e
yusuf <yusuf@DESKTOP-S7R0K6U>
2023-08-05 17:05:46 +0300

    Proje dosyası ekle.

---
+++ Değişkenler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Application
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(typeof(decimal).IsPrimitive);
+            Console.WriteLine(typeof(int).IsPrimitive);
+            Console.WriteLine(typeof(byte).IsPrimitive);
+
+        }
+    }
+}
